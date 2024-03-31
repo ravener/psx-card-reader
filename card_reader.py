4bc6564f3f94efb49fbe76982f3ad68578c29d08
Raven Ravener <ravener.anime@gmail.com>
2024-04-01 02:24:19 +0500

    refactor: round off decimals from size

--- card_reader.py
+++ card_reader.py
@@ -108,13 +108,13 @@ if __name__ == "__main__":
         sys.exit(1)
 
     directories = parse_header(read_block(data, 0))
-    print("File Name            | Size      | Blocks    | Title")
+    print("File Name            | Size   | Blocks    | Title")
     total_size = 0
 
     for i, directory in enumerate(directories):
         if directory.state == FIRST:
             name = directory.file_name.ljust(20)
-            size = f"{str(directory.file_size / 1024).center(6)} KB"
+            size = f"{str(directory.file_size // 1024).center(3)} KB"
             blocks = f"{str(directory.file_size // BLOCK_SIZE).center(2)} Block"
 
             # Make the word plural
